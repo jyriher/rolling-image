15ad5b1a5b2ea71289165d58890ea8f134bc63e3
jyriher <jyriher@metropolia.fi>
2018-12-09 13:43:55 +0200

    rolling image loppuviilaus

---
+++ rolling.js
@@ -0,0 +1,8 @@
+    //simply putting the wanted values to marquee class:    
+    $('.marquee').marquee({
+        duration: 20000,
+        gap: 1080,
+        delayBeforeStart: 0,
+        direction: 'up',
+        duplicated: true
+    });
